a83c2565dd4ea0e121465d0ecfc66927e76f7fe1
Anders Pedersen <hot415@hotmail.com>
2022-11-30 14:48:15 +0100

    Removed example folder The docs should now be a replacement for usage explanation

--- example/app/View/home.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <title>Home</title>
-    </head>
-    <body>
-        <App\Components\Card img="https://via.placeholder.com/50x50" text="test" />
-    </body>
-</html>
--- example/www/index2.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(__DIR__.'/../../vendor/autoload.php');
-
-header("Content-Type: text/plain");
-
-$view = new \Genius257\View\View(__DIR__.'/../app/View/home');
-
-echo $view->render();
